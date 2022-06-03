17f86c4c8e412816c3c2a80e9b0ec27a961076be
Achi <achiugulava@gamil.com>
2022-06-03 18:34:23 +0400

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,100 @@
+const logotext = "Achi Ugulava";
+const meta = {
+    title: "Achi Ugulava",
+    description: "I’m Achi Ugulava, self taught dveloper ",
+};
+
+const introdata = {
+    title: "I’m Achi Ugulava",
+    animated: {
+        first: "Web Developer",
+        second: "Coding For Fun",
+        third: "Problem Solver",
+    },
+    description: "Fullstack web developer from Georgia",
+    your_img_url: "https://gm1.ggpht.com/BIfOIT6XgkpVHNnF4oSTXgbfffg0VyGA1x2PB4cVlGlYkql8ry2OkqIoaWb8HFwXgkW-rmGrRqLUgoFkquU7XOZN4Cjia75_cBD2Gm-ix3zYR7U-nGSvIs5pHe1fRL-8OqFRLKkn91N2akN8EStk_25JXcd_WS_629JmH7Qv9OlwHzDomAZh0LHoWyhIFDkW5NX9B9OEmV2d9xqbTt6jsHiXWmFAS0sSL1fc6wNALNbQ4Jmw1pOLYMms0GsejC1Cn4HXvp6TzKjqi20VVl_6iE0aq7fh7D1mwaHtGoxutrHuyVavDAdV0dFSNrR9iGGzgdvjxmbUjCsrvxggxzDmQHG3iGCheXIkJtwA2LpWMJz1Pi3d4vVjsORsi8KZBuDMyEkw_bzERAafvG9PzG1K7Aal700jIZZFx2UxOhruhK_yKp42BpxgsPn60mKqozRjt6b0gCC7p1aXVdnmPn-ShB8lt9-F2qN0SIwGf9KsaSWgWXXTvTSBoO3U8ihnWDuIip47xe-8LmZVxwsXqluq8cp23OIrSSiZ1MibuFDKeAO1Cxsg4WFBVcaR2OrZna4fFrxQfQ62V2DuXsSI4XMI9k9yyNpJElTZPbIFvdlrgQ9llmNBR8ONWy_OdPf4WezVo7apAWmLsCBeO1tED6zHCT4JgVU-mGUHTME3Tv4RvikE5emq7NpVFGG8iXiSvjGidKm8sxrqDEsvtC3WoZ-vQezs42qPH8R9d-PKy9b7_Z9-Sl_Cv9AznrYkma0KDYrDn8prhgvkioY707TV=s0-l75-ft-l75-ft",
+};
+
+const dataabout = {
+    title: "about me",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+
+
+const skills = [{
+        name: "PHP",
+        value: 90,
+    },
+    {
+        name: "laravel",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "css",
+        value: 80,
+    },
+];
+
+const services = [{
+        title: "Front end",
+        description: "comfortable with CSS frameworks:tailwindCSS and bootstrap , knowledge of JS and basic understanding of React.",
+    },
+    {
+        title: "Back End",
+        description: "knowledge of PHP-Laravel framework , basics of node.JS-Express.JS .",
+    }
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        name:"Gunda bar",
+        desctiption: "e-commerce website for ice cream shop, built with laravel/livewire alpineJS and TailwindCSS",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        name:"Image detector",
+        desctiption: "static website built with react, lets you detect faces and food from the image.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        name:"Search user",
+        desctiption: "static website built with react, lets you search through the users",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "achiugulava@gmail.com",
+    description: "get in touch with me",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/AchiUgulava",
+    facebook: "https://www.facebook.com/Achiugulava/",
+    linkedin: "https://www.linkedin.com/in/achi-ugulava-38282a202/",
+    
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
